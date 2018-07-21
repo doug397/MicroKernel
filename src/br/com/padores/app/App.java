bcab80ea6348018f3780cc95be0232019ba0e696
Douglas Fonseca <douglas.vasco79@gmail.com>
2018-07-21 15:28:06 -0300

    Microkernel com patterns

--- src/br/com/padores/app/App.java
+++ src/br/com/padores/app/App.java
@@ -1,12 +1,13 @@
 package br.com.padores.app;
 import br.com.padores.interfaces.ICore;
+import br.com.padores.interfaces.IPlugin;
 import br.com.padores.nucleo.Core;
 
 public class App {
 
 	public static void main(String[] args) {
-		Core core = new Core();
-	
+		
+		ICore core = new Core();
 	    core.init();
 
 	}
